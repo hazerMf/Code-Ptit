dc1bbcfe746b5049e2fe5ffd9be0285b504ef66e
NVH <hazermfgobrrt@gmail.com>
2024-10-27 20:56:34 +0700

    update tcp server client

--- BT/ki12425/tcpClient.py
+++ BT/ki12425/tcpClient.py
@@ -3,8 +3,23 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET,SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
-sentence = input('Input lowercase sentence: ')
-clientSocket.send(sentence.encode())
-modifiedSentence = clientSocket.recv(1024)
-print(modifiedSentence.decode())
+
+a = input('Input number a: ')
+b = input('Input number b: ')
+
+clientSocket.send(str(a).encode())
+clientSocket.send(str(b).encode())
+
+results = clientSocket.recv(1024).decode().split('\n')
+
+for result in results:
+    if result:
+        print("Result:", result)
+
 clientSocket.close()
+
+#sentence = input('Input lowercase sentence: ')
+#clientSocket.send(sentence.encode())
+#modifiedSentence = clientSocket.recv(1024)
+#print(modifiedSentence.decode())
+#clientSocket.close()
--- BT/ki12425/tcpServer.py
+++ BT/ki12425/tcpServer.py
@@ -6,7 +6,23 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 while 1:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+
+    n1 = connectionSocket.recv(1024)
+    n2 = connectionSocket.recv(1024)
+
+    a = int(n1.decode())
+    b = int(n2.decode())
+
+    an1 = a+b
+    an2 = a-b
+    an3 = a*b
+    an4 = a/b
+
+    response = f"{an1}\n{an2}\n{an3}\n{an4}\n"
+
+    connectionSocket.send(response.encode())
+
+    #sentence = connectionSocket.recv(1024)
+    #capitalizedSentence = sentence.upper()
+    #connectionSocket.send(capitalizedSentence)
+    #connectionSocket.close()
